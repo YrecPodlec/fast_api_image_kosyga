97431619c9185cc86f676d7c63453e22d5f13d0f
YrecPodlec <domofon57@gmail.com>
2026-04-28 13:11:30 +0300

    cors fix

--- index.py
+++ index.py
@@ -8,7 +8,10 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://image-kosyga.vercel.app/"],
+    allow_origins=["http://localhost:3000",
+                   "http://image-kosyga.vercel.app",
+                   "https://image-kosyga.vercel.app"
+                   ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
